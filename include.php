0adf161bf0a61b1df40e0ed4d735abcd48cbea63
Derek Macias <derekmacias@live.com>
2014-10-17 00:16:04 -0600

    turn off debuging

--- include.php
+++ include.php
@@ -1,5 +1,5 @@
 <?php
-  // error_reporting(0);
+   error_reporting(0);
    require('/usr/local/emhttp/plugins/vmMan/classes/libvirt.php');
    require('/usr/local/emhttp/plugins/vmMan/classes/language.php');
 	$uri = 'qemu:///system';
