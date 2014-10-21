eaf7cb3066a405e407e007c22bb6a0f8b42d9448
Derek Macias <derekmacias@live.com>
2014-10-20 22:05:16 -0600

    changed to readonly

--- viewxml.php
+++ viewxml.php
@@ -1,12 +1,12 @@
 <div class="wrap">
 	<div class="list">
-	<h3>View XML Description</h3>
+	<h3>View Node XML Description</h3>
 		<div>
 			<form method="POST" id="viewXML">
 				<table>
 					<tr>
 						<td>
-							<textarea autofocus name="xmldesc" rows="16" cols="90%"><?php echo htmlentities($lv->get_node_device_xml($name, false));?></textarea>
+							<textarea autofocus readonly name="xmldesc" rows="16" cols="90%"><?php echo htmlentities($lv->get_node_device_xml($name, false));?></textarea>
 						</td>
 					</tr>
 					<tr>
