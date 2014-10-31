0e7d453b75234044803c050210bd7c579c71f1f0
Derek Macias <derekmacias@live.com>
2014-10-31 15:48:35 -0600

    combine cdrom and disks, change a couple vars

--- dominfo.php
+++ dominfo.php
@@ -2,7 +2,7 @@
   	<div class="list">
    <?php
 		$msg = "none";
-		$clrh = false;
+		$clear = false;
 		$uuid = $_GET['uuid'];
 		$domName = $lv->domain_get_name_by_uuid($uuid);
    	$res = $lv->get_domain_object($domName);
@@ -10,33 +10,31 @@
    	$dom = $lv->domain_get_info($res);
 		if ($action) {
 			if ($action == 'disk-remove') {
-				$lv->domain_disk_remove($domName, $_GET['dev']) ? 
-					$msg = 'Disk has been successfully removed' : 
-					$msg = 'Cannot remove disk: '.$lv->get_last_error();
-            $clrh = true; 
+				$msg = $lv->domain_disk_remove($domName, $_GET['dev']) ? 
+					'Disk has been successfully removed' : 
+					'Cannot remove disk: '.$lv->get_last_error();
 			}
 			elseif ($action == 'disk-add') {
 				$img = array_key_exists('img', $_POST) ? $_POST['img'] : false;
 				if ($img)
-					$lv->domain_disk_add($domName, $_POST['img'], $_POST['dev']) ? 
-						$msg = 'Disk has been successfully added to the guest' :
-						$msg = 'Cannot add disk to the guest: '.$lv->get_last_error();
-            $clrh = true; 
+					$msg = $lv->domain_disk_add($domName, $_POST['img'], $_POST['dev']) ? 
+						'Disk has been successfully added to the guest' :
+						'Cannot add disk to the guest: '.$lv->get_last_error();
 	 		}
 			elseif ($action == 'nic-remove') {
 				if ((array_key_exists('confirm', $_GET)) && ($_GET['confirm'] == 'yes'))
-					$lv->domain_nic_remove($domName, $_GET['mac']) ? 
-					$msg = 'Network card has been removed successfully' : 
-					$msg = 'Cannot remove network card: '.$lv->get_last_error();
-            $clrh = true; 
+					$msg = $lv->domain_nic_remove($domName, $_GET['mac']) ? 
+						'Network card has been removed successfully' : 
+						'Cannot remove network card: '.$lv->get_last_error();
 			}
 			elseif ($action == 'nic-add') {
 				$mac = array_key_exists('mac', $_POST) ? $_POST['mac'] : false;
 				if ($mac)
-					$lv->domain_nic_add($domName, $_POST['mac'], $_POST['network'], $_POST['model']) ? 
-					$msg = 'Network card has been successfully added to the guest' :
-					$msg = 'Cannot add NIC to the guest: '.$lv->get_last_error();
+					$msg = $lv->domain_nic_add($domName, $_POST['mac'], $_POST['network'], $_POST['model']) ? 
+						'Network card has been successfully added to the guest' :
+						'Cannot add NIC to the guest: '.$lv->get_last_error();
 			}
+		$clear = true;
 		}
 		$emulator = $lv->get_domain_emulator($domName);        
       $mem = number_format($dom['memory'] / 1024000, 1, '.', ' ').' GB';
@@ -89,11 +87,11 @@
 	        			</tr>
            		</table>";
 
-			//* Disk information */
+			//* Disk device information */
          echo "<h4><b>Disk devices</b><a href=\"?vmpage=dominfo&amp;uuid=$uuid&amp;action=disk-add\" title=\"not working yet\"><i class=\"glyphicon glyphicon-plus green\"></i></a></h4>";
             echo "<table class='table table-striped'>
          				<tr>
-            	      	<th>Disk storage</th>
+            	      	<th>Disk device</th>
                         <th> Storage driver type </th>
                         <th> Domain device </th>
                         <th> Disk capacity </th>
@@ -102,6 +100,7 @@
                    		<th> Actions </th>
                    	</tr>";
          $tmp = $lv->get_disk_stats($domName);
+         $tmp = array_merge($tmp,$lv->get_cdrom_stats($domName));
          if (!empty($tmp)) {
 				for ($i = 0; $i < sizeof($tmp); $i++) {
             	$capacity = $lv->format_size($tmp[$i]['capacity'], 2);
@@ -127,45 +126,6 @@
  			else
          	echo "<tr><td>Domain doesn't have any disk devices</td></tr>";
            echo "</table>";
-			//* cdrom information */
-         echo "<h4><b>CDRom devices</b><a href=\"?vmpage=dominfo&amp;uuid=$uuid&amp;action=disk-add\" title=\"not working yet\"><i class=\"glyphicon glyphicon-plus green\"></i></a></h4>";
-            echo "<table class='table table-striped'>
-         				<tr>
-            	      	<th>Disk storage</th>
-                        <th> Storage driver type </th>
-                        <th> Domain device </th>
-                        <th> Disk capacity </th>
-                   		<th> Disk allocation </th>
-                   		<th> Physical disk size </th>
-                   		<th> Actions </th>
-                   	</tr>";
-         $tmp = $lv->get_cdrom_stats($domName);
-         if (!empty($tmp)) {
-				for ($i = 0; $i < sizeof($tmp); $i++) {
-            	$capacity = $lv->format_size($tmp[$i]['capacity'], 2);
-               $allocation = $lv->format_size($tmp[$i]['allocation'], 2);
-               $physical = $lv->format_size($tmp[$i]['physical'], 2);
-               $dev = (array_key_exists('file', $tmp[$i])) ? $tmp[$i]['file'] : $tmp[$i]['partition'];
-
-					echo "<tr>
-               	     	<td>".basename($dev)."</td>
-                        <td align=\"left\">{$tmp[$i]['type']}</td>
-                        <td align=\"left\">{$tmp[$i]['device']}</td>
-                        <td align=\"left\">$capacity</td>
-                        <td align=\"left\">$allocation</td>
-                        <td align=\"left\">$physical</td>
-                        <td align=\"left\">
-                  	      <a href=\"?vmpage=dominfo&amp;uuid={$_GET['uuid']}&amp;action=disk-remove&amp;dev={$tmp[$i]['device']}\"
-                  	      onclick=\"return confirm('Disk is not deleted. Remove from domain?')\" title=\"remove disk from domain\">
-                  	      <i class=\"glyphicon glyphicon-remove red\"></i></a>
-                        </td>
-							</tr>";
-				}
-         }
- 			else
-         	echo "<tr><td>no cdrom devices</td></tr>";
-           echo "</table>";
-
 			/* Network interface information */
          echo "<h4><b>Network devices</b></h4>";
          	echo "<table class='table table-striped'>
@@ -205,7 +165,7 @@
          //if ( $dom['state'] == 1 ) {
          //    echo "<h3>Screenshots</h3><img src=\"screenshot.php?uuid={$_GET['uuid']}\">";
          //}
-		if ($clrh) echo "<script type=\"text/javascript\">	window.history.pushState('VMs', 'Title', '/VMs?vmpage=dominfo&uuid=$uuid'); </script>";
+		if ($clear) echo "<script type=\"text/javascript\">	window.history.pushState('VMs', 'Title', '/VMs?vmpage=dominfo&uuid=$uuid'); </script>";
    ?>
   	</div>
 </div>
