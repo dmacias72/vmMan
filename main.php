aac9b5662286ee90599c7f6ffb9f390cba9fd492
Derek Macias <derekmacias@live.com>
2014-11-08 14:41:05 -0700

    changed action and subaction format

--- main.php
+++ main.php
@@ -4,13 +4,21 @@
 	$msg = "none";
 	$clear = false;
 	$refresh = false;
-   if ($action) {
+	if ($action) {
+		if ($action == 'disk-add') {
+			include('/usr/local/emhttp/plugins/vmMan/addvol.php');
+		}
+		elseif ($action == 'domain-edit') {
+			include('/usr/local/emhttp/plugins/vmMan/editxml.php');
+		}
+	}else{
+   if ($subaction) {
      	$domName = $lv->domain_get_name_by_uuid($uuid);
-      if ($action == 'domain-start') {
+      if ($subaction == 'domain-start') {
         	$msg = $lv->domain_start($domName) ? "Domain $domName has been successfully started" : 
         		'Error while starting domain: '.$lv->get_last_error();
 			 }
-		elseif ($action == 'domain-autostart') {
+		elseif ($subaction == 'domain-autostart') {
 			$res = $lv->get_domain_by_name($name);
 	 		if ($lv->domain_get_autostart($res)) {
 	 			$msg = $lv->domain_set_autostart($res, false) ? "Domain $name has been successfully removed from autostart" :
@@ -20,59 +28,60 @@
        			'Error while setting domain to autostart: '.$lv->get_last_error(); 
 			}
 		}
-      elseif ($action == 'domain-pause') {
+      elseif ($subaction == 'domain-pause') {
         	$msg = $lv->domain_suspend($domName) ? 
   	      	"Domain $domName has been successfully paused" : 
    	     	'Error while pausing domain: '.$lv->get_last_error();
 		}
-      elseif ($action == 'domain-resume') {
+      elseif ($subaction == 'domain-resume') {
       	$msg = $lv->domain_resume($domName) ? 
       		"Domain $domName has been successfully resumed" : 
       		'Error while resuming domain: '.$lv->get_last_error();
 		}
-      elseif ($action == 'domain-restart') {
+      elseif ($subaction == 'domain-restart') {
         	$msg = $lv->domain_reboot($domName) ? 
         		"Domain $domName has been successfully restarted" : 
         		'Error while restarting domain: '.$lv->get_last_error();
 		}
-      elseif ($action == 'domain-save') {
+      elseif ($subaction == 'domain-save') {
         	$msg = $lv->domain_save($domName) ? 
-        		"Domain $domName has been successfully saved" : 
-        		'Error while saving domain: '.$lv->get_last_error();
+        		"Domain $domName has been successfully shutdown and saved" : 
+        		'Error while saving domain state: '.$lv->get_last_error();
 		}
-		elseif ($action == 'domain-stop') {
+		elseif ($subaction == 'domain-stop') {
          $msg = $lv->domain_shutdown($domName) ? 
          	"Domain $domName has been sent stop command" : 
          	'Error while stopping domain: '.$lv->get_last_error();
         	$refresh = true; }
-      elseif ($action == 'domain-destroy') {
+      elseif ($subaction == 'domain-destroy') {
          $msg = $lv->domain_destroy($domName) ? 
          	"Domain $domName has been successfully destroyed" : 
          	'Error while destroying domain: '.$lv->get_last_error();
       }
-      elseif ($action == 'domain-undefine') {
+      elseif ($subaction == 'domain-undefine') {
          $msg = $lv->domain_undefine($domName) ? "Domain $domName has been successfully undefined" : 
          	'Error while undefining domain: '.$lv->get_last_error();
       }
-      elseif ($action == 'domain-define') {
+      elseif ($subaction == 'domain-define') {
          if (@$_POST['xmldesc']) {
       	   $msg = $lv->domain_define( $_POST['xmldesc']) ? "Domain definition has been successfully added" : 
    	      	'Error adding domain definition: '.$lv->get_last_error();
-            }
-       }
-         //edit domain XML
-         elseif ($action == 'domain-create') {
-         	$inactive = (!$lv->domain_is_running($res, $name)) ? true : false;
-            $xml = $lv->domain_get_xml($domName, $inactive);
-           	if (@$_POST['xmldesc']) {
-           		$xml = $_POST['xmldesc'];
-              	$msg = $lv->domain_change_xml($domName, $xml) ? "Domain definition has been successfully changed" :
-                 	'Error changing domain definition: '.$lv->get_last_error();
-           	}
-			}
-		$clear=true;
+         }
+      }
+      //edit domain XML
+      elseif ($subaction == 'domain-create') {
+        	$inactive = (!$lv->domain_is_running($res, $name)) ? true : false;
+         $xml = $lv->domain_get_xml($domName, $inactive);
+         if (@$_POST['xmldesc']) {
+         	$xml = $_POST['xmldesc'];
+           	$msg = $lv->domain_change_xml($domName, $xml) ? "Domain definition has been successfully changed" :
+         	  	'Error changing domain definition: '.$lv->get_last_error();
+         }
 		}
+		$clear=true;
+	}
    $doms = $lv->get_domains();
+   sort($doms);
    $domkeys = array_keys($doms);
   	$tmp = $lv->get_domain_count();
    $active = $tmp['active'];
@@ -104,7 +113,7 @@
       $dom = $lv->domain_get_info($res);
       $mem = number_format($dom['memory'] / 1024000, 1, '.', ' ').' GB';
       $cpu = $dom['nrVirtCpu'];
-		$achk = $lv->domain_get_autostart($res) ? "checked":"";  
+		$auto = $lv->domain_get_autostart($res) ? "checked":"";  
       $state = $lv->domain_state_translate($dom['state']);
 		if ($state == 'running')
 		 	$scolor = 'LimeGreen';
@@ -134,7 +143,6 @@
       if (!$id)
         	$id = '-';
       unset($dom);
-
 		//Domain information
       echo "<tr>
         	      <td>
@@ -147,45 +155,48 @@
                <td>$arch</td>
                <td><font color=\"$scolor\">$state</font></td>
                <td>$id / $wsport</td>
-               <td><input type=\"checkbox\" title=\"Toggle VM auostart\" $achk onClick=\"javascript:location.href='?action=domain-autostart&amp;name=$name'\" ></td><td>";
+               <td><input type=\"checkbox\" title=\"Toggle VM auostart\" $auto onClick=\"javascript:location.href='?subaction=domain-autostart&amp;name=$name'\" ></td><td>";
 				
 		//Domain Action Buttons
       if ($lv->domain_is_running($res, $name)){
 			echo "<button class=\"btn btn-sm btn-primary\" onClick=\"window.open('$vnc','_blank','scrollbars=yes,resizable=yes'); return false;\" 
       	  			title=\"open VNC connection\"><i class=\"glyphicon glyphicon-eye-open\"></i></button> | 
-           		<button class=\"btn btn-sm btn-warning\" onClick=\"javascript:location.href='?action=domain-pause&amp;uuid=$uuid'\" 
+           		<button class=\"btn btn-sm btn-warning\" onClick=\"javascript:location.href='?subaction=domain-pause&amp;uuid=$uuid'\" 
         				title=\"Pause domain\"><i class=\"glyphicon glyphicon-pause\"></i></button> | 
-            	<button class=\"btn btn-sm btn-primary\" onClick=\"javascript:location.href='?action=domain-restart&amp;uuid=$uuid'\" 
+            	<button class=\"btn btn-sm btn-primary\" onClick=\"javascript:location.href='?subaction=domain-restart&amp;uuid=$uuid'\" 
         				title=\"restart domain\"><i class=\"glyphicon glyphicon-refresh\"></i></button> | 
-        			<button class=\"btn btn-sm btn-warning\" onClick=\"javascript:location.href='?vmpage=&amp;action=domain-save&amp;uuid=$uuid'\" 
+        			<button class=\"btn btn-sm btn-warning\" onClick=\"javascript:location.href='?subaction=domain-save&amp;uuid=$uuid'\" 
         				title=\"Suspend to disk, save domain state\"><i class=\"glyphicon glyphicon-save\"></i></button> | 
-        			<button class=\"btn btn-sm btn-danger\" onClick=\"javascript:location.href='?vmpage=&amp;action=domain-stop&amp;uuid=$uuid'\" 
+        			<button class=\"btn btn-sm btn-danger\" onClick=\"javascript:location.href='?subaction=domain-stop&amp;uuid=$uuid'\" 
         				title=\"safely shutdown domain\"><i class=\"glyphicon glyphicon-stop\"></i></button> | 
-              	<a class=\"btn btn-sm btn-default\" href=\"?action=domain-destroy&amp;uuid=$uuid\" 
-              		onClick=\"return confirm('Are your sure you want to force shutdown $name?')\" title=\"force domain to shutdown\"><i class=\"glyphicon glyphicon-eject\"></i></a>";
+              	<button class=\"btn btn-sm btn-default\" href=\"?subaction=domain-destroy&amp;uuid=$uuid\" 
+              		onClick=\"return confirm('Are your sure you want to force shutdown $name?')\" title=\"force domain to shutdown\"><i class=\"glyphicon glyphicon-eject\"></i></button>";
  		}else {
         	if ($state == "paused")
-        		echo "<button class=\"btn btn-sm btn-success\" onClick=\"javascript:location.href='?action=domain-resume&amp;uuid=$uuid'\" 
-        				title=\"resume domain\"><i class=\"glyphicon glyphicon-play\"></i></button>";
+        		echo "<button class=\"btn btn-sm btn-success\" onClick=\"javascript:location.href='?subaction=domain-resume&amp;uuid=$uuid'\" 
+        				title=\"resume domain\"><i class=\"glyphicon glyphicon-play\"></i></button> |
+        			<button class=\"btn btn-sm btn-warning\" onClick=\"javascript:location.href='?subaction=domain-save&amp;uuid=$uuid'\" 
+        				title=\"Suspend to disk, save domain state\"><i class=\"glyphicon glyphicon-save\"></i></button>";
 			else
-        		echo "<button class=\"btn btn-sm btn-success\" onClick=\"javascript:location.href='?action=domain-start&amp;uuid=$uuid'\" 
+        		echo "<button class=\"btn btn-sm btn-success\" onClick=\"javascript:location.href='?subaction=domain-start&amp;uuid=$uuid'\" 
         				title=\"start domain\"><i class=\"glyphicon glyphicon-play\"></i></button>";
 		}
       if ($lv->domain_is_running($res, $name))
-			echo " | <button class=\"btn btn-sm btn-info\" onClick=\"javascript:location.href='?vmpage=editxml&amp;uuid=$uuid&amp;view=readonly'\" 
+			echo " | <button class=\"btn btn-sm btn-info\" onClick=\"javascript:location.href='?action=domain-edit&amp;uuid=$uuid&amp;view=readonly'\" 
 		  		title=\"view domain XML\"><i class=\"glyphicon glyphicon-open\"></i></button>";
 		else
-      	echo " | <a class=\"btn btn-sm btn-danger\" href=\"?vmpage=main&amp;action=domain-undefine&amp;uuid=$uuid\" 
+      	echo " | <a class=\"btn btn-sm btn-danger\" href=\"?subaction=domain-undefine&amp;uuid=$uuid\" 
           		 onClick=\"return confirm('Are your sure you want to remove $name?')\" title=\"delete domain definition\"><i class=\"glyphicon glyphicon-remove\"></i></a>
-          		  | <button class=\"btn btn-sm btn-info\" onClick=\"javascript:location.href='?vmpage=editxml&amp;uuid=$uuid&amp;view='\" 
+          		  | <button class=\"btn btn-sm btn-info\" onClick=\"javascript:location.href='?action=domain-edit&amp;uuid=$uuid&amp;view='\" 
 		  		title=\"edit domain XML\"><i class=\"glyphicon glyphicon-plus\"></i></button>";
 		}
 	}
 	echo "</td></tr></table></div>";
 	if ($clear) echo "<script type=\"text/javascript\">	window.history.pushState('VMs', 'Title', '/VMs'); </script>";
-	if ($refresh) echo '<meta http-equiv="refresh" content="5; url=?vmpage=main&amp;refresh2=true">';
-	if ($_GET['refresh2']) echo '<meta http-equiv="refresh" content="7; url=?vmpage=main&amp;refresh=true">';
-	if ($_GET['refresh']) echo '<meta http-equiv="refresh" content="7; url=?vmpage=main">';
+	if ($refresh) echo '<meta http-equiv="refresh" content="3; url=?vmpage=&amp;refresh2=true">';
+	if ($_GET['refresh2']) echo '<meta http-equiv="refresh" content="5; url=?vmpage=&amp;refresh=true">';
+	if ($_GET['refresh']) echo '<meta http-equiv="refresh" content="7; url=/VMs">';
+}
 ?>
 	</div>
 </div>
